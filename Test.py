01488414540c91b5bd7a4dccc92ff54fb1ec7f70
Mohsen Fatahi <mohsen.fatahi@rashasoft.ir>
2019-02-13 00:37:55 +0330

    code for specify counts of each feature added.

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ filename = '/home/mohsen/Desktop/primaryPCI.csv'
 data = pd.read_csv(filename, sep=';')
 data = data[data.columns[0:64]]
 headers = list(data)
-s=data['GC1GeneralCharacteristicsMaritalStatus'].value_counts()
+s=data['GC1GeneralCharacteristicsSex'].value_counts()
 data = data.drop(['Patientid', 'encounterid', 'AdmissionAdmissionProfileNumber',
                   'PrimaryLast','نامبيمار', 'main', 'PMH1PastMedicalHistrySuccessfulCPR',
                   'DemographicDataDemographicSex', 'D41stLesionPCI1stLesionPCIProcedureType',
