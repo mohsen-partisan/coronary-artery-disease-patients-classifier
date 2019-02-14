ef0815e1455bfe13b0f3f340ff065bbc5a8ae1db
Mohsen Fatahi <mohsen.fatahi@rashasoft.ir>
2019-02-14 13:53:19 +0330

    all redundant columns dropped.

--- Test.py
+++ Test.py
@@ -5,15 +5,14 @@ import pandas as pd
 filename = '/home/mohsen/Desktop/primaryPCI.csv'
 data = pd.read_csv(filename, sep=';')
 data = data[data.columns[0:64]]
-print(data.shape)
 headers = list(data)
-s=data['ECG1ECGThirdDegree'].value_counts()
+s=data['GC1GeneralCharacteristicsOccupation'].value_counts()
 data = data.drop(['Patientid', 'encounterid', 'AdmissionAdmissionProfileNumber',
                   'PrimaryLast','نامبيمار', 'main',
                   'DemographicDataDemographicSex', 'D41stLesionPCI1stLesionPCIProcedureType',
+                  'GC1GeneralCharacteristicsInsuranceCo',
                   'InitialReperfusionTherapyTransferToCathlabRescuePCI',
                   'ECG1ECGThirdDegree', 'GC1GeneralCharacteristicsAdmision',
                   'PatientFullName'
                   ], axis=1)
-print(data.shape)
 print("ha")
