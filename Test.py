4c39fda266832d741029fd13f81f74ecf75f9ca0
Mohsen Fattahi <mohsen.fattahi@rashasoft.ir>
2019-02-17 11:02:32 +0330

    all blank and values contain '.' replaced with 'nan'.

--- Test.py
+++ Test.py
@@ -1,13 +1,11 @@
 
 import pandas as pd
 from sklearn import preprocessing
+import numpy as np
 
-
-filename = '/home/mohsen/Desktop/primaryPCI.csv'
+filename = '/home/mohsen/Desktop/pci/new/primaryPCI.csv'
 data = pd.read_csv(filename, sep=';')
 data = data[data.columns[0:64]]
-headers = list(data)
-s=data['D41stLesionPCI1stLesionPCIACCAHAType'].value_counts()
 data = data.drop(['Patientid', 'encounterid', 'AdmissionAdmissionProfileNumber',
                   'PrimaryLast','نامبيمار', 'main',
                   'DemographicDataDemographicSex', 'D41stLesionPCI1stLesionPCIProcedureType',
@@ -16,13 +14,15 @@ data = data.drop(['Patientid', 'encounterid', 'AdmissionAdmissionProfileNumber',
                   'ECG1ECGThirdDegree', 'GC1GeneralCharacteristicsAdmision',
                   'PatientFullName'
                   ], axis=1)
-data.dtypes
-for i in data['D41stLesionPCI1stLesionPCIACCAHAType']:
-    if i.empty:
-        data['D41stLesionPCI1stLesionPCIACCAHAType'] = 'NaN'
+headers = list(data)
+data = data.replace(r'^\s*$', np.nan, regex=True)
+data = data.replace('.', np.nan)
+s=data['MACE'].value_counts()
+miss = data['MACE'].isnull().sum()
+
 # encode categorical values
-le = preprocessing.LabelEncoder()
-for i in range(0,data.shape[1]):
-    if data.dtypes[i]=='object':
-        data[data.columns[i]] = le.fit_transform(data[data.columns[i]])
+# le = preprocessing.LabelEncoder()
+# for i in range(0,data.shape[1]):
+#     if data.dtypes[i]=='object':
+#         data[data.columns[i]] = le.fit_transform(data[data.columns[i]])
 print("ha")
